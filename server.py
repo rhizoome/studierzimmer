89e32054922789828b29e99d2bea44e728932ef8
Jean-Louis Fuchs <safe.pen2857@rhizoome.ch>
2025-01-04 11:46:01 +0100

    server that tells browser not to cache

---
+++ server.py
@@ -0,0 +1,17 @@
+import http.server
+import os
+import socketserver
+
+PORT = 8000
+
+class MyHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def end_headers(self):
+        self.send_header("Cache-Control", "no-cache, no-store, must-revalidate")
+        self.send_header("Pragma", "no-cache")
+        self.send_header("Expires", "0")
+        super().end_headers()
+
+os.chdir("build")
+with socketserver.TCPServer(("", PORT), MyHTTPRequestHandler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
